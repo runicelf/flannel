19def64d9f4195485133d5de0bf645f6f9bee01b
Oleg Perov <Oleg.Perov@infowatch.com>
2021-02-11 13:21:06 +0500

    fix err handler

--- network/iptables.go
+++ network/iptables.go
@@ -159,11 +159,12 @@ func ensureIPTables(ipt IPTables, rules []IPTablesRule) error {
 
 func setupIPTables(ipt IPTables, rules []IPTablesRule) error {
 	// Here we create a chain for forward rules
-	err := ipt.NewChain("filter", FlannelFwdChain)
-	eerr, ok := err.(*iptables.Error)
-	// Exit code 1 means the chain already exists
-	if ok && eerr.ExitCode() != 1 {
-		return fmt.Errorf("failed to create chain: %v", err)
+	if err := ipt.NewChain("filter", FlannelFwdChain); err != nil {
+		eerr, ok := err.(*iptables.Error)
+		// Exit code 1 means the chain already exists
+		if ok && eerr.ExitCode() != 1 {
+			return fmt.Errorf("failed to create chain: %v", err)
+		}
 	}
 
 	if err := appendRulesUniq(ipt, rules); err != nil {
